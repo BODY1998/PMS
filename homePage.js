ccd199473738c150ee5b77532ea92ae96b02f51c
BODY1998 <aymanabdo815@gmail.com>
2024-02-17 22:53:03 +0200

    implement getTotal function

---
+++ homePage.js
@@ -0,0 +1,39 @@
+let title = document.getElementById("title");
+let price = document.getElementById("price");
+let taxes = document.getElementById("taxes");
+let ads = document.getElementById("ads");
+let discount = document.getElementById("discount");
+let total = document.getElementById("total");
+let count = document.getElementById("count");
+let category = document.getElementById("category");
+let submit = document.getElementById("submit");
+
+// get total
+function getTotal() {
+  if (price.value != "") {
+    let result = +price.value + +taxes.value + +ads.value - +discount.value;
+    total.innerHTML = result;
+    total.style.background = "red";
+  } else {
+    total.innerHTML = "";
+    total.style.background = "rgba(67, 3, 187, 0.946)";
+  }
+}
+
+// create a product
+let products = [];
+
+submit.onclick = function () {
+  let newPro = {
+    title: title.value,
+    price: price.value,
+    taxes: taxes.value,
+    ads: ads.value,
+    discount: discount.value,
+    total: total.innerHTML,
+    count: count.value,
+    category: category.value,
+  };
+  products.push(newPro);
+  console.log(products);
+};
